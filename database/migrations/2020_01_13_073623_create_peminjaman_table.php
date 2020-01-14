d1c28ea65cdb13347d5773a09a0646f5418d9598
nisrina <niap1502@gmail.com>
2020-01-14 13:59:13 +0700

    Commit 1

---
+++ database/migrations/2020_01_13_073623_create_peminjaman_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeminjamanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_anggota');
+            $table->integer('id_petugas');
+            $table->date('tgl_pinjam');
+            $table->date('deadline');
+            $table->date('tgl_kembali');
+            $table->string('denda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+}
